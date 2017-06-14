601f69861c2df08ebc49fecb49279d2f7d49504a
wither23265fa <nish802108@gmail.com>
2017-06-14 14:33:49 +0800

    add facebook script

--- HW7_UpdateWeek5Bulid/app/js/script.js
+++ HW7_UpdateWeek5Bulid/app/js/script.js
@@ -16,3 +16,28 @@ $(document).ready(function() {
 
 	});
 });
+
+// facebook SDK
+window.fbAsyncInit = function() {
+	FB.init({
+		appId      : '1966131870300579',
+		cookie     : true,
+		xfbml      : true,
+		version    : 'v2.8'
+	});
+	FB.AppEvents.logPageView();
+};
+
+(function(d, s, id){
+	 var js, fjs = d.getElementsByTagName(s)[0];
+	 if (d.getElementById(id)) {return;}
+	 js = d.createElement(s); js.id = id;
+	 js.src = "//connect.facebook.net/en_US/sdk.js";
+	 fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
