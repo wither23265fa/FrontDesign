a7a0141216ecfdaede1d09169f708a13e62a9535
wither23265fa <nish802108@gmail.com>
2017-06-14 14:58:47 +0800

    套留言

--- HW7_UpdateWeek5Bulid/app/js/script.js
+++ HW7_UpdateWeek5Bulid/app/js/script.js
@@ -18,31 +18,31 @@ $(document).ready(function() {
 });
 
 // facebook SDK
-window.fbAsyncInit = function() {
-	FB.init({
-		appId      : '1966131870300579',
-		cookie     : true,
-		xfbml      : true,
-		version    : 'v2.8'
-	});
-	FB.AppEvents.logPageView();
-};
-
-(function(d, s, id){
-	 var js, fjs = d.getElementsByTagName(s)[0];
-	 if (d.getElementById(id)) {return;}
-	 js = d.createElement(s); js.id = id;
-	 js.src = "//connect.facebook.net/en_US/sdk.js";
-	 fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
-
- FB.getLoginStatus(function(response) {
-     statusChangeCallback(response);
- });
-
- 
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
+// window.fbAsyncInit = function() {
+// 	FB.init({
+// 		appId      : '1966131870300579',
+// 		cookie     : true,
+// 		xfbml      : true,
+// 		version    : 'v2.8'
+// 	});
+// 	FB.AppEvents.logPageView();
+// };
+//
+// (function(d, s, id){
+// 	 var js, fjs = d.getElementsByTagName(s)[0];
+// 	 if (d.getElementById(id)) {return;}
+// 	 js = d.createElement(s); js.id = id;
+// 	 js.src = "//connect.facebook.net/en_US/sdk.js";
+// 	 fjs.parentNode.insertBefore(js, fjs);
+//  }(document, 'script', 'facebook-jssdk'));
+//
+//  FB.getLoginStatus(function(response) {
+//      statusChangeCallback(response);
+//  });
+//
+//
+// function checkLoginState() {
+//   FB.getLoginStatus(function(response) {
+//     statusChangeCallback(response);
+//   });
+// }
