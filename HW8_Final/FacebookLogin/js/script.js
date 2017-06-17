fd3a3da35751739c8d88f6ef7025014814e2061d
wither23265fa <nish802108@gmail.com>
2017-06-17 19:46:27 +0800

    y

--- HW8_Final/FacebookLogin/js/script.js
+++ HW8_Final/FacebookLogin/js/script.js
@@ -41,15 +41,5 @@ $(document).ready(function() {
 
 	FB.getLoginStatus(function(response) {
 		statusChangeCallback(response);
-
-		if (response.status == 'connected') { // 登入成功
-			var txt1 = $("<p></p>").text(response.authResponse.userID);
-			var txt2 = $("<p></p>").text(response.authResponse);
-			var txt3 = $("<p></p>").text(response.status);
-			$userInfo.append(txt1, tx2, tx3);
-		} else {
-			var txt2 = $("<p></p>").text("none");
-			$userInfo.append("none");
-		}
 	});
 });
