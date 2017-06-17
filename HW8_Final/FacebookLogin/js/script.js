7f12dc7e8cf990b448212e2c21a729f63c982416
wither23265fa <nish802108@gmail.com>
2017-06-17 17:07:32 +0800

    wq

--- HW8_Final/FacebookLogin/js/script.js
+++ HW8_Final/FacebookLogin/js/script.js
@@ -1,37 +1,7 @@
-$(document).ready(function() {
-
-  // Get the modal
-  var modal = document.getElementById('id01');
-
-  // const $userInfo = $('.UserInfo');
-  // var userInfo = document.getElementsByClassName('UserInfo');
-  const $userInfo = $('#UserInfo');
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
-
-
-
-  // var text = $("<h2></h2>").text("123");
-  // var txt2 = $("<p></p>").text("Text.");
-
-
-  // userInfo.append(text);
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-
-    if (response.status == 'connected') { // 登入成功
-      var txt2 = $("<p></p>").text(response.authResponse.userID);
-      $userInfo.append(txt2);
-    } else {
-      var txt2 = $("<p></p>").text("none");
-      $userInfo.append("none");
-    }
-  });
-
-
-});
+// $(document).ready(function() {
+//
+//
+//
+//
+//
+// });
