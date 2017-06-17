0d6d95fb5a577f482f4f7935fab8eb539f402c50
wither23265fa <nish802108@gmail.com>
2017-06-17 21:32:18 +0800

    1

--- HW8_Final/FacebookLogin/js/script.js
+++ HW8_Final/FacebookLogin/js/script.js
@@ -9,7 +9,8 @@ $(document).ready(function() {
 		FB.AppEvents.logPageView();
 
 		FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
+    	statusChangeCallback(response);
+
   	});
 	};
 
@@ -23,6 +24,24 @@ $(document).ready(function() {
 		js.src = "//connect.facebook.net/en_US/sdk.js";
 		fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
+
+	function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else {
+      // The person is not logged into your app or we are unable to tell.
+      document.getElementById('UserInfo').innerHTML = 'Please log ' +
+        'into this app.';
+    }
+  }
+
 
 	// FB.getLoginStatus(function(response) {
 	// 	statusChangeCallback(response);
@@ -42,7 +61,7 @@ $(document).ready(function() {
 			modal.style.display = "none";
 		}
 	};
-	// 
+	//
 	// FB.getLoginStatus(function(response) {
 	// 	statusChangeCallback(response);
 	//
