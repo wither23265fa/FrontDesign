94d1a40b1db89248f8fb71524e6b6b1620d2fcc6
wither23265fa <nish802108@gmail.com>
2017-06-17 15:57:00 +0800

    增加jquery

--- HW8_Final/FacebookLogin/js/script.js
+++ HW8_Final/FacebookLogin/js/script.js
@@ -1,12 +1,17 @@
+$(document).ready(function() {
 // Get the modal
 var modal = document.getElementById('id01');
+
+// const $userInfo = $('.UserInfo');
+// var userInfo = document.getElementsByClassName('UserInfo');
+const $userInfo = $('#UserInfo');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
 	if (event.target == modal) {
 		modal.style.display = "none";
 	}
-}
+};
 
 window.fbAsyncInit = function() {
 	FB.init({
@@ -26,6 +31,22 @@ window.fbAsyncInit = function() {
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
+// var text = $("<h2></h2>").text("123");
+// var txt2 = $("<p></p>").text("Text.");
+
+
+// userInfo.append(text);
 FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
+
+		if (response.status == 'connected') { // 登入成功
+			var txt2 = $("<p></p>").text(response.authResponse.userID);
+			$userInfo.append(txt2);
+		}
+		else {
+			var txt2 = $("<p></p>").text("none");
+			$userInfo.append("none");
+		}
+});
+
 });
