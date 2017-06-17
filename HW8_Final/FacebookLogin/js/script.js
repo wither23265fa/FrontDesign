92691ba6ebb6255144eea3093dc0ea67fec09ad2
wither23265fa <nish802108@gmail.com>
2017-06-17 16:47:04 +0800

    update script to index

--- HW8_Final/FacebookLogin/js/script.js
+++ HW8_Final/FacebookLogin/js/script.js
@@ -1,22 +1,6 @@
-window.fbAsyncInit = function() {
-	FB.init({
-		appId      : '1966131870300579',
-		cookie     : true,
-		xfbml      : true,
-		version    : 'v2.8'
-	});
-	FB.AppEvents.logPageView();
-};
 
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
-// $(document).ready(function() {
+$(document).ready(function() {
 // Get the modal
 var modal = document.getElementById('id01');
 
@@ -51,4 +35,5 @@ FB.getLoginStatus(function(response) {
 		}
 });
 
-// });
+
+});
