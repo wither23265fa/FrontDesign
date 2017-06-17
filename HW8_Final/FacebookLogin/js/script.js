6a7a2e0b160870335e66f0ddfd18c40f0d19dd5b
wither23265fa <nish802108@gmail.com>
2017-06-17 16:52:18 +0800

    reset script and index

--- HW8_Final/FacebookLogin/js/script.js
+++ HW8_Final/FacebookLogin/js/script.js
@@ -1,39 +1,47 @@
-
-
 $(document).ready(function() {
-// Get the modal
-var modal = document.getElementById('id01');
 
-// const $userInfo = $('.UserInfo');
-// var userInfo = document.getElementsByClassName('UserInfo');
-const $userInfo = $('#UserInfo');
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId: '1966131870300579',
+      cookie: true,
+      xfbml: true,
+      version: 'v2.8'
+    });
+    FB.AppEvents.logPageView();
+  };
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-	if (event.target == modal) {
-		modal.style.display = "none";
-	}
-};
+  // Get the modal
+  var modal = document.getElementById('id01');
+
+  // const $userInfo = $('.UserInfo');
+  // var userInfo = document.getElementsByClassName('UserInfo');
+  const $userInfo = $('#UserInfo');
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 
 
 
-// var text = $("<h2></h2>").text("123");
-// var txt2 = $("<p></p>").text("Text.");
+  // var text = $("<h2></h2>").text("123");
+  // var txt2 = $("<p></p>").text("Text.");
 
 
-// userInfo.append(text);
-FB.getLoginStatus(function(response) {
+  // userInfo.append(text);
+  FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
 
-		if (response.status == 'connected') { // 登入成功
-			var txt2 = $("<p></p>").text(response.authResponse.userID);
-			$userInfo.append(txt2);
-		}
-		else {
-			var txt2 = $("<p></p>").text("none");
-			$userInfo.append("none");
-		}
-});
+    if (response.status == 'connected') { // 登入成功
+      var txt2 = $("<p></p>").text(response.authResponse.userID);
+      $userInfo.append(txt2);
+    } else {
+      var txt2 = $("<p></p>").text("none");
+      $userInfo.append("none");
+    }
+  });
 
 
 });
